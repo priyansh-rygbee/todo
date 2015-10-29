cae72f45bbe53ac1ca31151cf54af50879986fb1
priyansh-rygbee <priyansh.rygbee@gmail.com>
2015-10-30 02:49:48 +0530

    Found a way to interact with the system in achieving a persistent state. Now the script will try to read fromt the fie and skip the introductions for as long as possible

--- script.py
+++ script.py
@@ -1,3 +1,12 @@
-import todoist
-api = todoist.TodoistAPI('fc44bdb20cb968561390c39d85d3904c33f265d3')
-api.sync(resource_types=['all'])
+import sys
+import os
+home_dir = os.path.join(os.path.expanduser("~"))
+if '.todorc' in os.listdir(home_dir):
+	print "Found config file. Reading content, setting up user."
+
+print "Welcome to todo, the unofficial command line interface for Todoist.\nTo store your user settings and other preferences, we would need to create a '.todorc' file in your home directory.\n"
+if raw_input("Do you allow it? [y/n]")[0].lower() == 'y':
+	print "Creating .todorc"
+	config_file = open(os.path.join(os.path.expanduser("~"), '.todorc'),'w+')
+	
+
